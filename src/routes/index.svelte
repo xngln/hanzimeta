60506b35bcf47c65ebd48448c5cb080a4985b2e3
xngln <davxliu@gmail.com>
2022-02-17 17:35:59 -0500

    add static hanzi json data and use that for table

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,34 @@
 <script>
-
+    import hzjson from "../../static/data/hzdata.json"
 </script>
 
 <h1>hanzimeta</h1>
+<table>
+    <thead>
+        <tr>
+            <th>hanzi</th>
+            <th>pinyin</th>
+            <th>traditional</th>
+            <th>kanji</th>
+            <th>general standard #</th>
+            <th>frequency rank</th>
+            <th>hsk level</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each hzjson.hanzi as row}
+            <tr>
+                <td>{row.simplified}</td>
+                <td>{row.pinyin}</td>
+                <td>{row.traditional}</td>
+                <td>{row.kanji}</td>
+                <td>{row.gsNum}</td>
+                <td>{row.freq}</td>
+                <td>{row.hskLvl}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
 
 <style>
 
