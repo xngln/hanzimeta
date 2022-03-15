c1dfee8d76837673a898078b706afc4e76c042e7
xngln <davxliu@gmail.com>
2022-03-15 17:27:41 -0400

    switch to urql, move api url to env var

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,67 @@
-<h1 class="text-3xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { createClient, setClient, operationStore, query } from '@urql/svelte';
+
+    const client = createClient({
+        url: String(import.meta.env.VITE_GQL_API),
+    });
+
+    setClient(client);
+
+    let first = 10;
+    let hzPage = operationStore(`
+        query HanziConnection($first: Int) {
+            hanziConnection(first: $first) {
+                totalCount, 
+                pageInfo {
+                    endCursor,
+                    hasNextPage
+                },
+                edges {
+                    node {
+                        id,
+                        simplified,
+                    },
+                    cursor
+                }
+            }
+        }`, 
+        { first }
+    );
+
+    query(hzPage)
+</script>
+
+<h1 class="text-2xl">
+    hanzimeta
+</h1>
+<table class="table-auto">
+    <thead>
+        <tr>
+            <th>hanzi</th>
+            <th>pinyin</th>
+            <th>traditional</th>
+            <th>kanji</th>
+            <th>general standard #</th>
+            <th>frequency rank</th>
+            <th>hsk level</th>
+        </tr>
+    </thead>
+    <tbody>
+        <!-- {#each data.data.hanziConnection.edges as edge}
+            <tr>
+                <td>{edge.node.simplified}</td>
+                <td>{row.data().pinyin}</td>
+                <td>{row.data().traditional}</td>
+                <td>{row.data().kanji}</td>
+                <td>{row.data().gs_num}</td>
+                <td>{row.data().freq}</td>
+                <td>{row.data().hsk_lvl}</td>
+            </tr>
+        {/each} -->
+    </tbody>
+</table>
+
+<style>
+
+</style>
