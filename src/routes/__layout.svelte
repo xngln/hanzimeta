32c79eef598d386c4a2aba8437a7fd2b94b91cc9
xngln <davxliu@gmail.com>
2022-03-23 01:37:13 -0400

    add google analytics stuff

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,19 @@
 <script>
     import "../app.css";
 </script>
+
+<svelte:head>
+    {#if String(import.meta.env.VITE_ENV) === "PRODUCTION"}
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8F4P17TTJK"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-8F4P17TTJK');
+        </script>
+    {/if}
+</svelte:head>
   
 <slot />
